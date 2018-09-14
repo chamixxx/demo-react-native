517127a870d6a0fc5a4a7cdd1ac524bc6d8858d3
Othmane Chami khazraji <othmane.chami@gmail.com>
2018-09-14 14:38:44 +0200

    Deleted files

--- rn-cli.config.js
+++
@@ -1,9 +0,0 @@
-// rn-cli.config.js
-module.exports = {
-    getTransformModulePath() {
-      return require.resolve('react-native-typescript-transformer')
-    },
-    getSourceExts() {
-      return ['ts', 'tsx'];
-    }
-}
--- src/Utils/Mixpanel.tsx
+++
@@ -1,7 +0,0 @@
-import Mixpanel from "react-native-mixpanel";
-
-const mixpanel = Mixpanel.sharedInstanceWithToken(
-  "66b849f20ff344bd94fe312bb20bae7d"
-);
-
-export default mixpanel;
